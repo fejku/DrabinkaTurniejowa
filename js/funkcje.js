8094e7658b2749e750745d6852d096c26dc0d710
Krystian Fejku <przecinek45@o2.pl>
2014-03-04 21:50:05 +0100

    dziala wyswietlanie TODO: przy nacisnieciu na mecz mozlwiosc wprowadzenia wyniku (wiekszy wynik przechodzi dalej)

--- js/funkcje.js
+++ js/funkcje.js
@@ -5,12 +5,7 @@ $(function () {
     $('#btnDodaj').on('click', Dodaj);
     //Dodaje zdarzenie rysowania tabeli
     $('#btnRysuj').on('click', RysujTabele);
-    $('#ss1').on('click', ALA);
 });
-
-function ALA() {
-    KolejnaPot2($('#ss').val());
-}
 
 /**
  * Wyswietla listę uczestników
@@ -137,6 +132,7 @@ function Usun() {
     Wyswietl();
 }
 
+//Rysuje Drabinke
 function RysujTabele() {
     var tabela = $('#dDrabinka');
     
@@ -152,30 +148,56 @@ function RysujTabele() {
     gracze = JSON.parse(gracze);
     var iloscGraczy = gracze.length;
     var iloscRund = KtoraPotega2(iloscGraczy);
+    var wielkoscDrabinki = KolejnaPot2(iloscGraczy);
     var str = "";
+    
+    //Ile pustych miejsc w pierwszej rundzie
+    var iloscBrakujacych = wielkoscDrabinki - iloscGraczy;
+    var tabFreeWin = [];
+    for(var i = 0; i < iloscBrakujacych; i++) {
+        //losuje mecz w ktorym gracz ma wygrac automatycznie (ilosc graczy /2)
+        do {
+            var freeWin = Math.floor((Math.random()*(wielkoscDrabinki / 2))+1);
+        } while (tabFreeWin.indexOf(freeWin)!==-1);
+        tabFreeWin[i] = freeWin;
+    }
+    //Pomieszanie graczy miejscami
+    gracze = pomieszajTablice(gracze);
+    //Sortowanie liczb rosnaco
+    tabFreeWin.sort(function(a,b){return a-b;});
+    for(var i = 0; i < iloscBrakujacych; i++) {
+        //Automatyczna wygrana jest zawsze drugim zawodnikiem
+        tabFreeWin[i] = (tabFreeWin[i] * 2) - 1;
+        //Dodaj do tablicy przesuwajac kolejne elementy
+        gracze.splice(tabFreeWin[i], 0, 'Free Win');
+    }
     
     for(var j = 0; j < iloscRund; j++) {
         str += '<div id="r'+j+'" class="runda">' +
-               '<div class="naglowek">Runda 1</div>' +
+               '<div class="naglowek">Runda '+(j + 1)+'</div>' +
                '<div class="pierwOdst"></div>';
-        for (var i = 0; i < (KolejnaPot2(iloscGraczy) / DzielnikKolejneRundy(j)); i++) {
+        for (var i = 0; i < (wielkoscDrabinki / DzielnikKolejneRundy(j)); i++) {
             str += '<div class="gracz gracz'+i+'">' +
-                        '<div class="nazwa">' +
-                            gracze[i] +
-                        '</div>' +
-                        '<div class="wynik">' +
-                            0 +
-                        '</div>' +
-                        '<div class="kreski">' +
-                            '&nbsp;'+
-                        '</div>' +
-                    '</div>';
+                        '<div class="nazwa">';
+            //Wyswietlaj nazwy graczy tylko w 1 rundzie??
+            if(j == 0)
+                str += gracze[i];
+            //else
+                //str += '&nbsp;'
+            str += '</div>' +
+                    '<div class="wynik">' +
+                        0 +
+                    '</div>' +
+                    '<div class="kreski">' +
+                        '&nbsp;'+
+                    '</div>' +
+                '</div>';
 
             //Co dwoch graczy (jeden mecz)
             //i+1 bo zaczyna sie od 0
             if(!((i + 1) % 2)) {
                 //Nie rob odstepu po ostatnim meczu
-                if(i < ((KolejnaPot2(iloscGraczy) / DzielnikKolejneRundy(j)) - 1)) {
+                if(i < ((wielkoscDrabinki / DzielnikKolejneRundy(j)) - 1)) {
                     //Zrob odstep
                     str += '<div class="odstep odstep'+((i-1)/2)+'"></div>';
                 }
@@ -187,12 +209,12 @@ function RysujTabele() {
     
     tabela.append(str);
     
-    for(var i = 0; i < KolejnaPot2(iloscGraczy); i++) {
+    //Dodanie kresek (nie mozna we wczesniejszej petli bo nie wykrywa jeszcze tych elementow)
+    for(var i = 0; i < wielkoscDrabinki; i++) {
         //co drugi
         if(!((i + 1) % 2)) {
             //ale nie co czwarty
             if(((i + 1) % 4)) {
-                    tabela.find('.gracz'+ i).find('.kreski').css({'border-top': '1px solid black', 'border-right': '1px solid black'});
                     tabela.find('.gracz'+ i).find('.kreski').css({'border-top': '1px solid black', 'border-right': '1px solid black'});
                 }
         }
@@ -207,29 +229,39 @@ function RysujTabele() {
     
     //Ustawienie szerokosci tabeli na ilosc rund * szerokosc 
     //wszystkich elementow
-    //tabela.css('width', 2 * (margNazwa + szerNazwa + szerWyn + szerKresk));
+    tabela.css('width', iloscRund * (margNazwa + szerNazwa + szerWyn + szerKresk));
     
+    //Overflow poniewaz rundy maja float
     tabela.css({'overflow': 'hidden'});
     tabela.find('.runda').css({'width': margNazwa + szerNazwa + szerWyn + szerKresk, 'float': 'left'});
     
+    tabela.find('.naglowek').css({'font-weight': 'bold', 'padding-left': '10px', 'margin-top': '5px'});
+    
     tabela.find('.gracz').css({'overflow': 'hidden'});
     
-    tabela.find('.nazwa').css({'background-color': 'green', 'width': szerNazwa, 'margin-left': margNazwa, 'float': 'left'});
-    tabela.find('.wynik').css({'width': szerWyn, 'text-align': 'right', 'float': 'left'});
-    tabela.find('.kreski').css({'width': szerKresk, 'float': 'left'});
+    tabela.find('.nazwa').css({'background-color': 'green', 'width': szerNazwa, 'height': wysPole, 'margin-left': margNazwa, 'float': 'left'});
+    tabela.find('.wynik').css({'width': szerWyn, 'height': wysPole, 'text-align': 'right', 'float': 'left'});
+    tabela.find('.kreski').css({'width': szerKresk, 'height': wysPole, 'float': 'left'});
     tabela.find('.pierwOdst').css({'height': wysPierwOdst});
     tabela.find('.odstep').css({'height': wysOdst});
     $('.runda .odstep:nth-child(odd)').css({'border-right': '1px solid black'});
-    var poprzWys = 10;
+    //w 1 rundzie 
+    var poprzWys = wysPierwOdst;
+    //Ustawienie pierwszych odstepow i odstepow w kolejnych runach
     for(var i = 1; i < iloscRund; i++) {
+        //Ustawienie pierwszego odstepu w kolejych rundach
         tabela.find('#r'+i).find('.pierwOdst').css({'height': (wysPole + (2 * poprzWys))});
+        //parse poniewaz zwracanyc byl string z *px
         poprzWys = parseInt(tabela.find('#r'+ i ).find('.pierwOdst').css('height'), 10);
+        //odstep jest 2x pierwszy odstep
         tabela.find('#r'+i).find('.odstep').css({'height': (poprzWys * 2)});
     }
+    //w ostaniej rundzie nie wyswietlaj kresek
     tabela.find('#r'+(iloscRund-1)).find('.kreski').css({'border': 0});
 }
 
-var KolejnaPot2 = function (liczba) {
+
+function KolejnaPot2(liczba) {
     var pom = 1;
 
     //Zwraca zawsze kolejna potege 2 nawet jesli liczba
@@ -240,7 +272,8 @@ var KolejnaPot2 = function (liczba) {
     return pom;
 }
 
-var KtoraPotega2 = function (liczba) {
+//Zwraca do koterej potegi musi byc podniesiona 2 aby otrzymac dana liczbe
+function KtoraPotega2(liczba) {
     var pom = 1, i = 0;
     do {
         i++;
@@ -249,11 +282,23 @@ var KtoraPotega2 = function (liczba) {
     return i;
 }
 
-var DzielnikKolejneRundy = function (runda){
+//Pomocnicza funkcja zwracajaca kolejne potegi 2
+function DzielnikKolejneRundy(runda){
     var pom = 1;
     for(var i = 0; i < runda; i++)
     {
         pom *= 2;
     }
     return pom;
+}
+
+//Pomieszanie tablicy przy uzyciu algorytmu Fisher-Yates
+function pomieszajTablice(tab) {
+    for (var i = tab.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = temp;
+    }
+    return tab;
 }
